60ba15953c10751e4d3386be69c1eda12eba56fc
Swarna Dharshini S <swarnadharshinis@gmail.com>
2025-10-01 15:46:23 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 import "./Style.css";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
     <TailoringStitcher />
-  </HashRouter>
 );
 function TailoringStitcher() {
   const [item, setItem] = useState("");
